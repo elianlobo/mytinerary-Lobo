82f7705ab82eb23abfd4c6b02d4e010b60aa189c
Elian Lobo <elianlobo24@gmail.com>
2022-06-07 17:56:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './styles/App.css';
+import NavBar from './components/NavBar'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import { Card } from '@mui/material';
+
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Body/>
+    <Card/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/style.css'
+
+function Body(){
+    return (
+        <div className="body">
+        <h1>
+        "Find your perfect trip, designed by insiders who know and love their cities!"
+        </h1>
+        </div>
+    )
+}
+
+export default Body
